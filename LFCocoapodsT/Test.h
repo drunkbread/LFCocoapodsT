3daa85b988b1f45616f70455a53269f55440f33b
lifei <lifei9241@126.com>
2018-08-07 10:51:07 +0800

    init lib

---
+++ LFCocoapodsT/Test.h
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  d
+//
+//  Created by 李飞 on 2018/8/7.
+//  Copyright © 2018年 李飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
++ (void)test;
+@end
---
+++ LFCocoapodsT/Test.m
@@ -0,0 +1,17 @@
+//
+//  Test.m
+//  d
+//
+//  Created by 李飞 on 2018/8/7.
+//  Copyright © 2018年 李飞. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
++ (void)test
+{
+    NSLog(@"biu~");
+}
+@end
